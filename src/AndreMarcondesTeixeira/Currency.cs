d4bf6543c8d2d58ce532923c85e27868e8a8d621
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2017-06-28 22:37:26 -0300

    starts documenting code and begins preparing new tests for future changes

--- src/AndreMarcondesTeixeira/Currency.cs
+++ src/AndreMarcondesTeixeira/Currency.cs
@@ -2,24 +2,65 @@ using System;
 
 namespace AndreMarcondesTeixeira
 {
+    /// <summary>
+    ///     The <see cref="Currency" /> class follows ISO 4217:2015, providing an easy way
+    ///     to represent ISO currencies.
+    /// </summary>
+    /// <remarks>
+    ///     All currencies are represented, except for "ANTARCTICA", "PALESTINE, STATE OF"
+    ///     and "SOUTH GEORGIA AND THE SOUTH SANDWICH ISLANDS". ISO describes those as
+    ///     "no universal currency", and gives them no letter codes.
+    ///     <para>This class does not provide the list of countries that use each currency.</para>
+    ///     <para>
+    ///         For more information:
+    ///         <see href="https://www.currency-iso.org/en/home.html">
+    ///             https://www.currency-iso.org/en/home.html
+    ///         </see> and
+    ///         <see href="https://www.iso.org/standard/64758.html">
+    ///             https://www.iso.org/standard/64758.html
+    ///         </see>
+    ///     </para>
+    /// </remarks>
     public partial class Currency
     {
+        /// <summary>The 3 letters ISO code of the currency</summary>
         public string Code { get; }
+
+        /// <summary>The ISO minor units of the currency</summary>
         public byte MinorUnits { get; }
+
+        /// <summary>The ISO name of the currency</summary>
         public string Name { get; }
+
+        /// <summary>The numeric ISO code of the currency</summary>
         public string Number { get; }
 
         /// <summary>
-        /// Use the static, lazy loaded properties of
-        /// the Currency class instead of the constructor
+        ///     Initializes a new instance of the <see cref="Currency" /> class.
         /// </summary>
+        /// <remarks>
+        ///     Instead of using the constructor, consider using the static, lazy loaded
+        ///     properties of the <see cref="Currency" /> class instead.
+        ///     <para>
+        ///         For a list of the available currencies, use IntelliSense, or:
+        ///         <see href="https://www.currency-iso.org/en/home.html">
+        ///             https://www.currency-iso.org/en/home.html
+        ///         </see>
+        ///     </para>
+        /// </remarks>
         /// <example>
-        /// This sample shows you how to get an instance of a currency:
-        /// <code>
-        /// var currency = Currency.XXX;
-        /// </code>
+        ///     This sample shows you how to get an instance of a currency, where "XXX" is
+        ///     the 3 letters ISO code of the currency:
+        ///     <code>
+        ///         var currency = Currency.XXX;
+        ///         var customCurrency = new Currency("ZZZ", "999", 0, "Custom Currency");
+        ///     </code>
         /// </example>
-        private Currency(string code, string number, byte minorUnits, string name)
+        /// <param name="code">The 3 letters ISO code of the Currency.</param>
+        /// <param name="number">The numeric ISO code of the Currency.</param>
+        /// <param name="minorUnits">The ISO minor units of the Currency.</param>
+        /// <param name="name">The ISO name of the Currency.</param>
+        public Currency(string code, string number, byte minorUnits, string name)
         {
             this.Code = code;
             this.Number = number;
--- test/AndreMarcondesTeixeira/CurrencyTest.cs
+++ test/AndreMarcondesTeixeira/CurrencyTest.cs
@@ -6,15 +6,15 @@ namespace AndreMarcondesTeixeira
     public class CurrencyTest
     {
         [Fact]
-        public void Currency_Instances_Are_Compared_Through_Their_ISONumber()
+        public void Currency_Instances_Are_Compared_Through_Their_Numeric_ISO_Code()
         {
-            var inexistentCurrency = new Currency("inexistent 1", "000", 2, "inexistent");
-            var anotherInexistentCurrency = new Currency("inexistent1 ", "000", 0, "bar");
+            var customCurrency1 = new Currency("", "000", 0, "");
+            var customCurrency2 = new Currency("ZZZ", "000", 2, "Test");
 
+            Assert.True(customCurrency1 == customCurrency2);
+            Assert.False(customCurrency1 != customCurrency2);
             Assert.False(Currency.XXX == Currency.XTS);
             Assert.True(Currency.XTS != Currency.XXX);
-            Assert.True(inexistentCurrency == anotherInexistentCurrency);
-            Assert.False(inexistentCurrency != anotherInexistentCurrency);
         }
 
         [Fact]
@@ -36,6 +36,27 @@ namespace AndreMarcondesTeixeira
 
             var exception5 = Assert.Throws<ArgumentException>(() => null != Currency.XTS);
             Assert.Equal(message, exception5.Message);
+        }
+
+        [Fact]
+        public void Currency_Instances_Have_Read_Only_Properties()
+        {
+
+        }
+
+        [Fact]
+        public void Currency_Instances_Can_Be_Created_By_Four_Ways()
+        {
+            Assert.IsType(typeof(Currency), Currency.XXX);
+            Assert.IsType(typeof(Currency), new Currency("ZZZ", "000", 0, "Test"));
+            Assert.IsType(typeof(Currency), Currency.GetByLetterCode("XXX"));
+            Assert.IsType(typeof(Currency), Currency.GetByNumericCode("999"));
+        }
+
+        [Fact]
+        public void All_Factory_Methods_Return_New_Instances()
+        {
+            
         }
     }
 }
