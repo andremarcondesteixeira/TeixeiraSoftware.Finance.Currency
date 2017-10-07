8a8e271c40dbbe4f754ca7a2b9825beb14d405df
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2017-10-07 10:12:21 -0300

    Reformats some code and adds an assertion about comparing two instances of Currency

--- test/AndreMarcondesTeixeira/CurrencyTest.cs
+++ test/AndreMarcondesTeixeira/CurrencyTest.cs
@@ -10,8 +10,14 @@ namespace AndreMarcondesTeixeira
         public void Currency_Is_A_Value_Type_And_All_Factory_Methods_Return_New_Instances()
         {
             Assert.False(Object.ReferenceEquals(Currency.XXX, Currency.XXX));
-            Assert.False(Object.ReferenceEquals(Currency.GetByLetterCode("XXX"), Currency.GetByLetterCode("XXX")));
-            Assert.False(Object.ReferenceEquals(Currency.GetByNumericCode("999"), Currency.GetByNumericCode("999")));
+            Assert.False(
+                Object.ReferenceEquals(Currency.GetByLetterCode("XXX"),
+                Currency.GetByLetterCode("XXX"))
+            );
+            Assert.False(
+                Object.ReferenceEquals(Currency.GetByNumericCode("999"),
+                Currency.GetByNumericCode("999"))
+            );
         }
 
         [Fact]
@@ -23,7 +29,7 @@ namespace AndreMarcondesTeixeira
         }
 
         [Fact]
-        public void Currency_Getter_Methods_Throws_Exception_For_Nonexistent_Currencies()
+        public void Currency_Getter_Methods_Throw_Exception_For_Nonexistent_Currencies()
         {
             Assert.Throws<ArgumentException>(() => Currency.GetByLetterCode("Nonexistent"));
             Assert.Throws<ArgumentException>(() => Currency.GetByNumericCode("Nonexistent"));
@@ -32,6 +38,15 @@ namespace AndreMarcondesTeixeira
         [Fact]
         public void Currency_Instances_Are_Compared_Through_Their_Whole_Set_Of_Properties()
         {
+            Assert.True(
+                Currency.XXX,
+                new Currency(
+                    "XXX",
+                    "999",
+                    0,
+                    "The codes assigned for transactions where no currency is involved"
+                )
+            );
             Assert.False(Currency.XXX == Currency.XTS);
             Assert.True(Currency.XTS != Currency.XXX);
         }
